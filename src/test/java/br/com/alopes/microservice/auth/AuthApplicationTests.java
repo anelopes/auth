3ea192aa75ac3049c978db867a88e271f65bdd26
anelopes <anelopes17@outlook.com>
2021-04-02 22:48:17 -0300

    Microservice - Project Auth

---
+++ src/test/java/br/com/alopes/microservice/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alopes.microservice.auth;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
