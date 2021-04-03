f6aced1287dcd3ec209a33d4750431745783ba1c
anelopes <anelopes17@outlook.com>
2021-04-03 20:04:27 -0300

    Alter project name

--- src/test/java/br/com/alopes/microservice/auth/AuthApplicationTests.java
+++ src/test/java/br/com/alopes/microservice/auth/AuthApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthApplicationTests {
+public class AuthApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
